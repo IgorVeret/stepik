96c7084ac234b7cfc03d8b1238ec43a065748bdb
igor <iveretennikov77@gmail.com>
2022-11-04 12:58:01 +0300

    Задача решена

--- 12.1 Домашнее задание. Основы машинного обучения/task_05.py
+++ 12.1 Домашнее задание. Основы машинного обучения/task_05.py
@@ -1,18 +1,19 @@
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import GridSearchCV
+
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 all_data = pd.read_csv('forest_dataset.csv')
-all_data.head()
+
 labels = all_data[all_data.columns[-1]].values
 feature_matrix = all_data[all_data.columns[:-1]].values
+
 train_feature_matrix, test_feature_matrix, train_labels, test_labels = train_test_split(
     feature_matrix, labels, test_size=0.2, random_state=42)
-# создание модели
 
+# создание модели
 clf = KNeighborsClassifier()
 optimal_clf = KNeighborsClassifier(n_neighbors=4, weights='distance', metric='manhattan')
 
@@ -30,4 +31,5 @@ plt.bar(range(1, 8), freq, width=-0.4, align="edge", label='real')
 plt.ylim(0, 0.54)
 plt.legend()
 plt.show()
+
 print(round(pred_freq[2], 2))
